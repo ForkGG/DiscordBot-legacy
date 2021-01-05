3cc66f1374c9ff5125760c60f309165d1db7b36d
Alpha Coder <38115697+Alpha-c0d3r@users.noreply.github.com>
2021-01-05 03:12:27 +0300

    Improved checks

--- DiscordBot/Modules/Bot Tools.cs
+++ DiscordBot/Modules/Bot Tools.cs
@@ -38,25 +38,26 @@ public class Bot_Tools : InteractiveBase
         {
            await Context.Message.DeleteAsync();
             var msg = await ReplyAsync("Alright give me few seconds please.");
-            if (!((bool)server.CheckAuth(token, Context.Guild.Id) == true))
+            if ((!(bool)server.CheckAuth(token, Context.Guild.Id) == true) && (!(bool)server.CheckOnhold(token) == false))
             {
                 //sorting the token goes here
                 //After connection if server replies, then its ok
-               // Checkmsg(token); //check if its in connection
-
+                string ip = (string)server.GetIPForToken(token);
+                CheckConnection(ip); //check if its in connection
                 if ((bool)check == true)
                 {
-                    server.InsertAuth(Context.Guild.Id, token);
+                    server.InsertAuth(Context.Guild.Id, token,ip);
+                    server.RemoveOnhold(token);
                     await msg.ModifyAsync(msgProperty => msgProperty.Content = "Great, your discord server is now authorized with your fork server.");
                 }
                 else
                 {
-                    await msg.ModifyAsync(msgProperty => msgProperty.Content = "Couldn't Connect To Your Fork Server, make sure its running.");
+                    await msg.ModifyAsync(msgProperty => msgProperty.Content = "Couldnt connect to your fork server, make sure its running.");
                 }
             }
             else
             {
-                await msg.ModifyAsync(msgProperty => msgProperty.Content = "Sorry, but this discord server or the token is already authorized.");
+                await msg.ModifyAsync(msgProperty => msgProperty.Content = "Sorry, but this discord server or the token is already authorized or invalid.");
             }
           
         }
@@ -65,12 +66,9 @@ public class Bot_Tools : InteractiveBase
             Console.WriteLine(ex.ToString());
         }
     }
-    private void CheckConnection(string msgg)
+    private void CheckConnection(string ip)
     {
- 
-      //var socket = Discord_Bot.allSockets.Any(client => client.ConnectionInfo.ClientIpAddress == "");
-
-        if (Discord_Bot.allSockets.Any(client => client.ConnectionInfo.ClientIpAddress == ""))
+        if (Discord_Bot.allSockets.Any(client => client.ConnectionInfo.ClientIpAddress == ip))
         {
             check = true;
         }
