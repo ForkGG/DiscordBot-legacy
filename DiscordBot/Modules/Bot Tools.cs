4a1e82bc4eae1c1cf17f177f6474d2d95c9f3460
Alpha Coder <38115697+Alpha-c0d3r@users.noreply.github.com>
2021-01-05 21:58:42 +0300

    Responds switched to embed for better look & removed and replaced some useless functions

--- DiscordBot/Modules/Bot Tools.cs
+++ DiscordBot/Modules/Bot Tools.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Addons.Interactive;
 using Discord.Commands;
 using Fleck;
@@ -10,11 +11,44 @@ using Websocket.Client;
 
 public class Bot_Tools : InteractiveBase
     {
-    public Server server = new Server();
+    public Discord.Embed Embed(string msg)
+    {
+        var ebd = new EmbedBuilder();
+        Color Colorr = new Color(21, 22, 34);
+        ebd.Color = Colorr;
+        ebd.WithDescription($"{msg}");
+        return ebd.Build();
 
+    }
+    public Server server = new Server();
+    [Command("stop", RunMode = RunMode.Async)]
+    [Alias("stop")]
+    [Summary("Stops your mc server. usage: (prefix)stop [worldname]")]
+    public async Task stop(string worldname)
+    {
+        try
+        {
+            var msg = await ReplyAsync(Context.Message.Author.Mention, false, Embed("Alright give me few seconds please."));
+            string token = (string)server.GetTokenOfServer(Context.Guild.Id);
+            string ip = (string)server.GetIPForToken(token,2);
+            if ((bool)CheckConnection(ip) == true)  //check if its connected
+            {
+                
+                await msg.ModifyAsync(msgProperty =>
+                {
+                    msgProperty.Content = $"{Context.Message.Author.Mention}";
+                    msgProperty.Embed = Embed("Great, your discord server is now authorized with your fork server.");
+                });
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
     [Command("ping", RunMode = RunMode.Async)]
         [Alias("latency")]
-        [Summary("Shows the websocket connection's latency and time it takes to send a message.")]
+        [Summary("Shows the websocket connection's latency and time it takes to send a message. usage: (prefix)ping")]
         public async Task PingAsync()
         {
             try
@@ -28,36 +62,47 @@ public class Bot_Tools : InteractiveBase
           
             }
         }
-    public bool check = false;
     [Command("auth", RunMode = RunMode.Async)]
     [Alias("authorize")]
-    [Summary("Authorizes your discord server with fork mc server")]
+    [Summary("Authorizes your discord server with fork mc server. usage: (prefix)auth [token]")]
     public async Task auth(string token)
     {
         try
         {
+
            await Context.Message.DeleteAsync();
-            var msg = await ReplyAsync("Alright give me few seconds please.");
+ var msg = await ReplyAsync(Context.Message.Author.Mention,false,Embed("Alright give me few seconds please."));
             if ((!(bool)server.CheckAuth(token, Context.Guild.Id) == true) && (!(bool)server.CheckOnhold(token) == false))
             {
                 //sorting the token goes here
                 //After connection if server replies, then its ok
-                string ip = (string)server.GetIPForToken(token);
-                CheckConnection(ip); //check if its in connection
-                if ((bool)check == true)
+                string ip = (string)server.GetIPForToken(token,1);
+                if ((bool)CheckConnection(ip) == true)  //check if its connected
                 {
                     server.InsertAuth(Context.Guild.Id, token,ip);
                     server.RemoveOnhold(token);
-                    await msg.ModifyAsync(msgProperty => msgProperty.Content = "Great, your discord server is now authorized with your fork server.");
+                    await msg.ModifyAsync(msgProperty =>
+                    {
+                        msgProperty.Content = $"{Context.Message.Author.Mention}";
+                        msgProperty.Embed = Embed("Great, your discord server is now authorized with your fork server.");
+                        });
                 }
                 else
                 {
-                    await msg.ModifyAsync(msgProperty => msgProperty.Content = "Couldnt connect to your fork server, make sure its running.");
+                    await msg.ModifyAsync(msgProperty =>
+                    {
+                        msgProperty.Content = $"{Context.Message.Author.Mention}";
+                        msgProperty.Embed = Embed("Couldnt connect to your fork server, make sure its running.");
+                    });
                 }
             }
             else
             {
-                await msg.ModifyAsync(msgProperty => msgProperty.Content = "Sorry, but this discord server or the token is already authorized or invalid.");
+                await msg.ModifyAsync(msgProperty =>
+                {
+                    msgProperty.Content = $"{Context.Message.Author.Mention}";
+                    msgProperty.Embed = Embed("Sorry, but this discord server or the token is already authorized or invalid.");
+                });
             }
           
         }
@@ -66,30 +111,42 @@ public class Bot_Tools : InteractiveBase
             Console.WriteLine(ex.ToString());
         }
     }
-    private void CheckConnection(string ip)
+    private bool CheckConnection(string ip)
     {
         if (Discord_Bot.allSockets.Any(client => client.ConnectionInfo.ClientIpAddress == ip))
         {
-            check = true;
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
     [Command("unauth", RunMode = RunMode.Async)]
     [Alias("unauthorize")]
-    [Summary("Unauthorizes your discord server with fork mc server")]
+    [Summary("Unauthorizes your discord server with fork mc server. usage: (prefix)unauth")]
     public async Task unauth()
     {
         try
         {
-            var msg = await ReplyAsync("Alright give me few seconds please.");
+            var msg = await ReplyAsync(Context.Message.Author.Mention, false, Embed("Alright give me few seconds please."));
             if (((bool)server.CheckAuth("", Context.Guild.Id) == true))
             {
                 server.RemoveAuth(Context.Guild.Id);
                 server.RemoveRole(Context.Guild.Id);
-                await msg.ModifyAsync(msgProperty => msgProperty.Content = "Your discord server got unlinked successfully.");
+                await msg.ModifyAsync(msgProperty =>
+                {
+                    msgProperty.Content = $"{Context.Message.Author.Mention}";
+                    msgProperty.Embed = Embed("our discord server got unlinked successfully.");
+                });
             }
             else
             {
-                await msg.ModifyAsync(msgProperty => msgProperty.Content = "Your discord server isnt linked.");
+                await msg.ModifyAsync(msgProperty =>
+                {
+                    msgProperty.Content = $"{Context.Message.Author.Mention}";
+                    msgProperty.Embed = Embed("Your discord server isnt linked.");
+                });
             }
             }
         catch (Exception ex)
